a37938f5fcf4ad9aacbd6add14dfe22a95514629
Franz Elwyn Anicas <anicasfranz@gmail.com>
2026-04-06 15:24:18 +0800

    .

--- config/settings.py
+++ config/settings.py
@@ -1,7 +1,10 @@
 import os
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = get_random_secret_key()
 
 DEBUG = True
 ALLOWED_HOSTS = [
